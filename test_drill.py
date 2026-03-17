521b8f8d210099647817162351517a83af84a064
eshraq allawama <allawamaeshraq@gmail.com>
2026-03-17 11:16:15 +0300

    Run the funstions and test all

--- test_drill.py
+++ test_drill.py
@@ -11,15 +11,17 @@ from drill_functions import clean_column, compute_revenue
 
 
 def test_clean_column():
-    # TODO: Create a pd.Series with at least one NaN value
-    # TODO: Call clean_column() on it
-    # TODO: Assert no NaN values remain in the result
-    # TODO: Assert the NaN was filled with the correct median value
+    series = pd.Series([4, 2, 4, np.NaN])
+    cleaned_data =  clean_column(series)
+    assert not series.isna().any()
+    assert 5 in series
     pass
 
 
 def test_compute_revenue():
-    # TODO: Create two small pd.Series (quantity and price)
-    # TODO: Call compute_revenue() on them
-    # TODO: Assert the result matches the expected element-wise product
+    series_quantity = [1, 2 3, 4]
+    series_price = [1, 2 3, 4]
+    result = compute_revenue(series_quantity, series_price)
+    expected = pd.Series([10, 40, 90])
+    assert result.equals(expected)  
     pass
